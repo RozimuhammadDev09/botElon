45ebd5b2a47be9344f525c147722ff378ecc3a23
Rozimuhammad <example@gmail.com>
2026-09-17 12:16:34 +0500

    yangi api

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ o'zgartirishingiz mumkin — boshqa fayllarga tegishning hojati yo'q.
 from pathlib import Path
 
 # ==================== BOT ====================
-TOKEN = "8212255968:AAGT8vMZo87J1Kdm73ejbv23TfBEwlEqY9o"
+TOKEN = "8212255968:AAG_FkxDvbzP7qf6m3rbuliF_EXD_54aGks"
 BOT_USERNAME = "RishtonBuvaydaBogdod_bot"  # @ belgisiz
 
 # ==================== ADMIN ====================
